ce4d4204fc3a980062b7b6882454d87ec301bd28
Felix Cordero Jr. <corderofelix101@gmail.com>
2024-05-17 17:09:08 +0800

    initial test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './home.html',
+  templateUrl: './app.home.html',
   styleUrls: ['../demo-styling.css']
 })
 export class AppComponent {
